0d0198ef67ddaa80513d76df544bda603ec459e4
hanna-uler <hannauler@gmail.com>
2025-06-11 13:19:45 -0400

    fix formik inputs

--- src/components/LoginForm/LoginForm.jsx
+++ src/components/LoginForm/LoginForm.jsx
@@ -8,14 +8,14 @@ import { login } from '../../redux/auth/operations';
 
 export default function LoginForm() {
     const loginForm = {
-        name: '',
+        email: '',
         password: '',
     };
     const dispatch = useDispatch();
-    const nameId = useId();
+    const emailId = useId();
     const passwordId = useId();
     const loginSchema = Yup.object().shape({
-        name: Yup.string().min(3, "Name must be at least 3 characters long.").max(50, "Name can't be longer than 50 characters.").required("Name is required."),
+        email: Yup.string().email().required("Email is required."),
         password: Yup.string().min(7, "Password must be at least 7 characters long.").required("Password is required.")
     });
 
@@ -30,9 +30,9 @@ export default function LoginForm() {
             validationSchema={loginSchema}
         >
             <Form className={css.form}>
-                <label className={css.label} htmlFor={nameId}>Name</label>
-                <Field className={css.field} type="text" name="name" id={nameId} />
-                <ErrorMessage className={css.error} name="name" component="span" />
+                <label className={css.label} htmlFor={emailId}>Email</label>
+                <Field className={css.field} type="email" name="email" id={emailId} />
+                <ErrorMessage className={css.error} name="email" component="span" />
                 <label className={css.label} htmlFor={passwordId}>Password</label>
                 <Field className={css.field} type="password" name="password" id={passwordId} />
                 <ErrorMessage className={css.error} name="password" component="span" />
