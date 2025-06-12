ef4bcca5c156bbc2ac15495f1c9fa3175ede5296
hanna-uler <hannauler@gmail.com>
2025-06-12 17:36:38 -0400

    add home page markup

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -3,8 +3,14 @@ import css from './HomePage.module.css'
 export default function HomePage() {
     return (
         <div className={css.container}>
-            <h1>Welcome</h1>
-            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ex aliquid quae non deserunt amet repellendus similique officiis tempore? At vero amet nostrum deleniti asperiores consequuntur corporis aperiam quis obcaecati exercitationem. Deserunt cupiditate ut suscipit maxime obcaecati? Omnis ipsam eligendi culpa.</p>
+            <h1 className={css.header}>Welcome to Phonebook</h1>
+            <p className={css.descr}>
+                Phonebook is a simple and secure contact management app that allows you to register, log in, and keep your personal contact list in the cloud.
+                Add new contacts, search by name, or remove outdated entries — all synced with your private backend.
+            </p>
+            <p className={css.descr}>
+                Start by <button className={css.btn} type='button'>Creating an account</button> or <button className={css.btn} >Logging in</button> to access your phonebook.
+            </p>
         </div>
     )
 }
