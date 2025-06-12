16b652f1ebc40916a5ab51aa0ba0e7d7eecd443b
hanna-uler <hannauler@gmail.com>
2025-06-12 10:43:09 -0400

    add persist storage

--- src/main.jsx
+++ src/main.jsx
@@ -2,19 +2,22 @@ import 'modern-normalize'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
+import { PersistGate } from 'redux-persist/integration/react'
 import './index.css'
 import App from './components/App/App.jsx'
 import '@fontsource/inter/400.css'
 import '@fontsource/inter/700.css'
 import { Provider } from 'react-redux'
-import { store } from './redux/store';
+import { store, persistor } from './redux/store';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
+      <PersistGate loading={null} persistor={persistor}>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </PersistGate>
     </Provider>
   </StrictMode>,
 );
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,12 +1,37 @@
 import { configureStore } from "@reduxjs/toolkit";
+import {
+    persistStore,
+    persistReducer,
+    FLUSH,
+    REHYDRATE,
+    PAUSE,
+    PERSIST,
+    PURGE,
+    REGISTER } from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
 import contactsReducer from "./contacts/slice";
 import filtersReducer from "./filters/slice";
 import authReducer from "./auth/slice";
+
+const persistConfig = {
+    key: 'user-token',
+    storage,
+    whitelist: ["token"],
+  }
+const persistedAuthReducer = persistReducer(persistConfig, authReducer)
 
 export const store = configureStore({
     reducer: {
         contacts: contactsReducer,
         filters: filtersReducer,
-        auth: authReducer,
+        auth: persistedAuthReducer,
     },
+    middleware: (getDefaultMiddleware) =>
+        getDefaultMiddleware({
+          serializableCheck: {
+            ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+          },
+        }),
 });
+
+export const persistor = persistStore(store);
