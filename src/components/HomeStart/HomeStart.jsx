e6a31feda52cd881ae5a1dba4ef07ee0b7ae7158
hanna-uler <hannauler@gmail.com>
2025-06-13 12:06:06 -0400

    upd HomeStart comp css

--- src/components/HomeStart/HomeStart.jsx
+++ src/components/HomeStart/HomeStart.jsx
@@ -4,13 +4,11 @@ import { Link } from 'react-router-dom'
 export default function HomeStart() {
     return (
         <div>
-            <p className={css.descr}>Start by</p>
+            <p className={css.descr}>Start by <strong>creating an account</strong> or <strong>logging in</strong> to access your phonebook.</p>
             <div className={css.btnBox}>
-            <Link to="/register" className={css.linkBtn}>Creating an account</Link>
-                <span className={css.span}>or</span>
-            <Link to="/login" className={css.linkBtn}>Logging in</Link>
+                <Link to="/register" className={css.linkBtn}>Create an account</Link>
+                <Link to="/login" className={css.linkBtn}>Log in</Link>
             </div>
-            <p className={css.descr}>to access your phonebook.</p>
         </div>
     )
 }
